916168082aeade281cd479780f40107f4a140ae5
Abhishek <abhishekyadavfeb1498@gmail.com>
2021-07-07 22:27:20 +0530

    home Page

---
+++ src/components/feed/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const Feed = () => {
+  return (
+    <div>
+      <span>
+        <h3>Home</h3>
+        <img src='' alt='internet_image' />
+      </span>
+			<div>
+			<img src=''alt='pc'/>
+			<span>What's Happening</span>
+			<div>Yaha Icon Ayyenge</div>
+			<div>Write</div>
+			</div>
+			<div>Others Feed</div>
+    </div>
+  );
+};
+export default Feed;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Login from  '../login/Login';
 import LeftNavBar from '../navBar/LeftNavBar'
 import RightNavBar from '../navBar/RightNavBar';
+import Feed from '../feed/Feed'
 
 const Home = () => {
   return (
     <div>
-    <RightNavBar/>
+    <Feed/>
     </div>
   );
 };
